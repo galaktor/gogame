47aacf5d440605c0e044fb14e5e34023b6c00365
galaktor <galaktor@galaktor>
2012-09-08 21:57:30 +0100

    some toy experiments with scene

--- system/system_test.go
+++ system/system_test.go
@@ -3,9 +3,29 @@ package system
 import (
 	"github.com/orfjackal/gospec"
 	. "github.com/orfjackal/gospec"
-)
+	"../scene")
+
+type MySystem struct {
+
+}
+
+type MyProperty struct {
+
+}
+
+func (p *MyProperty)Type() scene.PropertyType {
+	return scene.PropertyType(1)
+}
+
+
+
 
 func StartSpec(c gospec.Context) {
+	s := scene.NewScene()
+
+	s.Add("a", &MyProperty{})
+
+
 	c.Specify("I NEED TESTS!", func() {
 		c.Expect(true, Equals, false)
 	})
